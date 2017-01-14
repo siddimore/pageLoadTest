d6f5886de073215f86f7a25b87deef13f0bf9a1c
Siddharth More <siddharthmore@Siddharths-MacBook-Pro.local>
2017-01-13 22:07:17 -0800

    Take arguments as parameter

--- selenium/browserTime.py
+++ selenium/browserTime.py
@@ -1,5 +1,5 @@
 import time
-import sys
+import sys,getopt
 import subprocess
 import csv
 import emailmodule
@@ -9,42 +9,106 @@ CONFIG_FILE = "url_list.txt"
 NUM_ITERATION = 1
 RESULT_FILE='result.csv'
 
-def ui_check(url,n):
-    driver = webdriver.Chrome()
-    tot=0
-    for i in range(n):
-        start = time.time()
-        driver.get(url)
-        end = time.time()
-        tot=tot+(end-start)
-    driver.quit()
-    avgAccessTime=tot/n
-    return avgAccessTime
+class BrowserTest(object):
 
-def transport_check(url, n):
-    tot=0
-    for i in range(n):
-        t = float(subprocess.Popen("curl -w \"%{time_total}\" -o /dev/null -s " + '\"' + url + '\"', shell=True,
-                               stdout=subprocess.PIPE).stdout.read())
-        tot = tot + t
-    avgAccessTime=tot/n
-    return avgAccessTime
+    testUrl = ''
+    curlTime = 0.0
+    endtoendTime = 0.0
+    diffTime = 0.0
 
-def main():
-    f = open(CONFIG_FILE, 'r')
-    if not f:
-        printf("Cannot open file: " + CONFIG_FILE)
-        sys.exit()
-    resultFile=open(RESULT_FILE,'wb')
-    csvResult=csv.writer(resultFile)
-    csvResult.writerow(["URL","Transport Access Time","UI Access Time","Difference","UI Overhead %"])
-    for line in f:
-        line=line.strip()
-        transportTime = transport_check(line,10)
-        uiTime=ui_check(line,10)
-        uiOverhead=uiTime - transportTime
-        csvResult.writerow([line,str(transportTime),str(uiTime),str(uiOverhead),str(int(uiOverhead/uiTime*100))])
-    
-    emailmodule.sendEmail('xxxx@@gmail.com','xxxx')
+    def __init__(self, url):
+        self.url = url
+
+    def getCurlTime(self):
+        return self.curlTime
+
+    def getEndToTime(self):
+        return self.endtoendTime
+
+    def getdiffTime(self):
+        self.diffTime = self.endtoendTime - self.curlTime
+        return self.diffTime
+
+    def ui_check(self, n):
+        driver = webdriver.Chrome()
+        tot = 0
+        for i in range(n):
+            start = time.time()
+            driver.get(self.url)
+            end = time.time()
+            tot = tot + (end - start)
+        driver.quit()
+        avgAccessTime = tot / n
+        self.endtoendTime = avgAccessTime
+        return avgAccessTime
+
+    def transport_check(self, n):
+        tot = 0
+        for i in range(n):
+            t = float(subprocess.Popen("curl -w \"%{time_total}\" -o /dev/null -s " + '\"' + self.url + '\"', shell=True,
+                                       stdout=subprocess.PIPE).stdout.read())
+            tot = tot + t
+        avgAccessTime = tot / n
+        self.curlTime = avgAccessTime
+        return avgAccessTime
+
+def main(argv):
+    userName = ''
+    passWord = ''
+
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["username=", "pwd="])
+    except getopt.GetoptError:
+        print 'browserTest.py -i <username> -o <pwd>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'browserTest.py -i <username@gmail.com> -o <pwd>'
+            sys.exit()
+        elif opt in ("-i", "--username"):
+            userName = arg
+        elif opt in ("-o", "--pwd"):
+            passWord = arg
+    print 'UserName is:', userName
+    print 'PassWord is:', passWord
+
+    if userName:
+        if passWord:
+            # Read Data from Config File
+            f = open(CONFIG_FILE, 'r')
+            if not f:
+                printf("Cannot open file: " + CONFIG_FILE)
+                sys.exit()
+            resultFile=open(RESULT_FILE,'wb')
+            # Create CSV File to store Data for Operations
+            csvResult=csv.writer(resultFile)
+            csvResult.writerow(["URL","Transport Access Time","UI Access Time","Difference","UI Overhead %"])
+
+            # Iterate over each instance of url in Config file
+            for line in f:
+                line=line.strip()
+
+                # Create BrowserTest Object
+                browserTest = BrowserTest(line)
+                # Run Test with curl operations time for curl operations
+                browserTest.transport_check(10)
+                # Run Test by launching Browser and Load Url
+                browserTest.ui_check(10)
+
+                # Get time for Curl Operations
+                transportTime = browserTest.getCurlTime()
+                # Get time for Browser Launch operation
+                uiTime = browserTest.getEndToTime()
+                # Get diff time for Curl and Browser Test
+                uiOverhead = browserTest.getdiffTime()
+
+                # Write Data to csv file
+                csvResult.writerow([line,str(transportTime),str(uiTime),str(uiOverhead),str(int(uiOverhead/uiTime*100))])
+
+            emailmodule.sendEmail(userName,passWord)
+        else:
+            print "Input password please!!"
+    else:
+        print "Input username please!!"
 if __name__ == '__main__':
-  main()
+  main(sys.argv[1:])
--- selenium/emailmodule.py
+++ selenium/emailmodule.py
@@ -17,8 +17,36 @@ def sendEmail(username,password):
     body = 'Msg Body'
     msg.attach(MIMEText(body, 'plain'))
 
-#emailmessage = "My First Email With Python"
+    html = "<html><body><table><tr><td>name</td><td>age</td><td>dob</td<tr></table></body></html>"
+    msg1 = MIMEText(html, 'html')
+    msg = MIMEMultipart("test")
+    msg.attach(msg1)
+#emailmessage = "My First Email With Python To Booboo I love You!!"
     text = msg.as_string()
-    server.sendmail("siddimore@gmail.com", "siddimore@gmail.com", text)
+
+    # text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.python.org"
+    # html = """\
+    # <html>
+    #   <head></head>
+    #   <body>
+    #     <p>Hi!<br>
+    #        How are you?<br>
+    #        Here is the <a href="http://www.python.org">link</a> you wanted.
+    #     </p>
+    #   </body>
+    # </html>
+    # """
+    #
+    # # Record the MIME types of both parts - text/plain and text/html.
+    # part1 = MIMEText(text, 'plain')
+    # part2 = MIMEText(html, 'html')
+    #
+    # # Attach parts into message container.
+    # # According to RFC 2046, the last part of a multipart message, in this case
+    # # the HTML message, is best and preferred.
+    # msg.attach(part1)
+    # msg.attach(part2)
+    server.sendmail("siddimore@gmail.com", "siddimore@gmail.com", msg.as_string())
+    # server.sendmail("siddimore@gmail.com", "luxiaoxue@gmail.com", text)
 
     server.quit()
